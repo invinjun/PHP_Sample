d839c0ff5900567288ddb28fdeec3f8438dbd656
xiejun <invinjun@gmail.com>
2018-01-31 10:45:20 +0800

    第一次测试提交代码

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+  <title>Sample App</title>
+
+</head>
+<body>
+  <h1>关于</h1>
+</body>
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,16 @@
+
+/**
+ * Created by PhpStorm.
+ * User: xiejun
+ * Date: 2018/1/30
+ * Time: 上午11:21
+ */
+ <html>
+ <head>
+   <title>Sample App</title>
+
+ </head>
+ <body>
+   <h1>帮助</h1>
+ </body>
+ </html>
